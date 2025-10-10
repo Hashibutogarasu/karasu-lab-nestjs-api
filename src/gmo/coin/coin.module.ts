ef55fc3bd5725c2229563d8cf53a69dfd18d6bf2
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-10-10 12:27:03 +0900

    feat: Import AuthModule in CoinModule for authentication integration

--- src/gmo/coin/coin.module.ts
+++ src/gmo/coin/coin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoinController } from './coin.controller';
 import { CoinService } from './coin.service';
+import { AuthModule } from '../../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [CoinController],
   providers: [CoinService],
 })
